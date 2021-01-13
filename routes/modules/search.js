07ccc74488a664231d31e73b40296e80bd424c36
Ben Chou <ben7152000@gmail.com>
2021-01-13 16:50:29 +0800

    update search

--- routes/modules/search.js
+++ routes/modules/search.js
@@ -5,9 +5,9 @@ const restaurants = require('../../restaurant')
 router.get('/', (req, res) => {
   const keyword = req.query.keyword
   const restaurant = restaurants.results.filter(item => {
-    return item.name.toLowerCase().includes(keyword.toLowerCase()) || item.category.includes(keyword) || item
+    const word = item.name.toLowerCase().includes(keyword.toLowerCase()) || item.category.includes(keyword)
+    return word
   })
-
   res.render('index', { restaurant, keyword })
 })
 
